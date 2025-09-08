de2829c9decdd820a003220cd889e05857928a23
codetalehaha <githup2301@gmail.com>
2025-09-08 23:22:22 +0700

    update fragment home

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,22 +32,30 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
     implementation("androidx.gridlayout:gridlayout:1.0.0")
-    implementation(libs.camera.core)
-    implementation(libs.camera.camera2)   // 🔥 bắt buộc
-    implementation(libs.camera.lifecycle)
-    implementation(libs.camera.view)
+
+    // CameraX (chắc chắn có đủ core + camera2 + lifecycle + view)
+    implementation("androidx.camera:camera-core:1.2.3")
+    implementation("androidx.camera:camera-camera2:1.2.3")   // 🔥 bắt buộc
+    implementation("androidx.camera:camera-lifecycle:1.2.3")
+    implementation("androidx.camera:camera-view:1.2.3")
+
+    // Google Play Services
     implementation(libs.play.services.maps)
     implementation(libs.play.services.location)
-    implementation(libs.camera.view)
-    implementation(libs.camera.lifecycle)
+
+    // Navigation
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+
+    // Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
